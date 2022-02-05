7d67629a1582f325152eb53876bc684ce7c1dc48
lpelczar <lukepelczar@gmail.com>
2022-02-05 19:10:00 +0100

    Initial commit

---
+++ src/main/java/legacyfigher/dietary/DietaryApplication.java
@@ -0,0 +1,13 @@
+package legacyfigher.dietary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietaryApplication.class, args);
+	}
+
+}
