5da64b2b8417d4235810c28f29825ff6dca7a40c
lpelczar <lukepelczar@gmail.com>
2022-02-06 11:08:47 +0100

    Add product description unit tests

---
+++ src/test/java/legacyfigher/dietary/newproducts/ProductDescriptionTest.java
@@ -0,0 +1,58 @@
+package legacyfigher.dietary.newproducts;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDescriptionTest {
+
+    @Test
+    void shouldFormatDescription() {
+        // expect
+        assertEquals(aProduct("Nicely crafted beans", "Coffee").formatDesc(), "Coffee *** Nicely crafted beans");
+        assertEquals(aProduct("", "Coffee").formatDesc(), "");
+        assertEquals(aProduct("Nicely crafted beans", "").formatDesc(), "");
+        assertEquals(aProduct("Nicely crafted beans", null).formatDesc(), "");
+        assertEquals(aProduct(null, "Coffee").formatDesc(), "");
+        assertEquals(aProduct(null, null).formatDesc(), "");
+        assertEquals(aProduct(null, "").formatDesc(), "");
+        assertEquals(aProduct("", null).formatDesc(), "");
+        assertEquals(aProduct("", "").formatDesc(), "");
+    }
+
+    @Test
+    void shouldReplaceCharacterInDescription() {
+        //given
+        OldProduct product = aProduct("Nicely crafted Coffee", "Coffee");
+
+        //when
+        product.replaceCharFromDesc("Coffee", "Beans");
+
+        //then
+        assertEquals(product.formatDesc(), "Beans *** Nicely crafted Beans");
+    }
+
+    @Test
+    void shouldFailReplacingCharacterInDescription() {
+        //expect
+        assertThrows(IllegalStateException.class,
+                () -> aProduct("", "Coffee").replaceCharFromDesc("Coffee", "Beans"));
+        assertThrows(IllegalStateException.class,
+                () -> aProduct("Nicely crafted Coffee", "").replaceCharFromDesc("Coffee", "Beans"));
+        assertThrows(IllegalStateException.class,
+                () -> aProduct("", null).replaceCharFromDesc("Coffee", "Beans"));
+        assertThrows(IllegalStateException.class,
+                () -> aProduct(null, "").replaceCharFromDesc("Coffee", "Beans"));
+        assertThrows(IllegalStateException.class,
+                () -> aProduct(null, null).replaceCharFromDesc("Coffee", "Beans"));
+        assertThrows(IllegalStateException.class,
+                () -> aProduct("", "").replaceCharFromDesc("Coffee", "Beans"));
+    }
+
+    OldProduct aProduct(String longDesc, String desc) {
+        return new OldProduct(new BigDecimal(1), desc, longDesc, 1);
+    }
+
+}
